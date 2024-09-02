fa9c31e2f8199f56c23b1f3d7ab4d213d3a042dc
Dongho Shin <kakkiid@gmail.com>
2024-09-02 16:59:40 +0900

    feat: Tutorial15

---
+++ src/components/Tutorial15.vue
@@ -0,0 +1,23 @@
+<script setup>
+import JSConfetti from 'js-confetti';
+
+const confetti = new JSConfetti();
+
+function showConfetti() {
+  confetti.addConfetti();
+}
+
+showConfetti();
+</script>
+
+<template>
+  <h1 @click="showConfetti">🎉 축하합니다!</h1>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+}
+</style>
