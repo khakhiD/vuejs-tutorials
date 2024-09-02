9df9cea47c6af5c13940196fbe7ac8569bd278e0
Dongho Shin <kakkiid@gmail.com>
2024-09-02 16:42:47 +0900

    feat: Tutorial9

---
+++ src/components/Tutorial9.vue
@@ -0,0 +1,14 @@
+<!-- 생명주기와 템플릿 참조 -->
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const pElementRef = ref(null);
+
+onMounted(() => {
+  pElementRef.value.textContent = '마운트 되었어요!';
+});
+</script>
+
+<template>
+  <p ref="pElementRef">안녕</p>
+</template>
