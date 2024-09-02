25a64315eeaf0c75bfb7c64f7f44fecc56c1d538
Dongho Shin <kakkiid@gmail.com>
2024-09-02 16:43:01 +0900

    feat: Tutorial10

---
+++ src/components/Tutorial10.vue
@@ -0,0 +1,26 @@
+<!-- 감시자(watch) -->
+<script setup>
+import { ref, watch } from 'vue';
+
+const todoId = ref(1);
+const todoData = ref(null);
+
+async function fetchData() {
+  todoData.value = null;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  );
+  todoData.value = await res.json();
+}
+
+fetchData();
+
+watch(todoId, fetchData);
+</script>
+
+<template>
+  <p>Todo Id: {{ todoId }}</p>
+  <button @click="todoId++" :disabled="!todoData">다음 할 일 가져오기</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
+</template>
