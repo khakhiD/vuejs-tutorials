ef62ccc76077bf56d32706bc96090f7afdff2bf5
Dongho Shin <kakkiid@gmail.com>
2024-09-02 16:59:29 +0900

    feat: Tutorial14

---
+++ src/components/Tutorial14.vue
@@ -0,0 +1,11 @@
+<!-- 슬롯(slot) -->
+<template>
+  <ChildComponent><slot>slot asdf</slot>부모로부터: {{ msg }}</ChildComponent>
+</template>
+<script setup>
+import { ref } from 'vue';
+import { TutorialChild as ChildComponent } from './TutorialChild.vue';
+
+const msg = ref('Vue는 개발자에게 정말 유용하죠!');
+</script>
+<style lang=""></style>
