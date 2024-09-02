375143bb31a340a496fbdce48a82b36c52fd2eb4
Dongho Shin <kakkiid@gmail.com>
2024-09-02 16:52:46 +0900

    feat: Tutorial13 - Emit

---
+++ src/components/Tutorial13.vue
@@ -0,0 +1,12 @@
+<!-- Emits -->
+<script setup>
+import { ref } from 'vue';
+import { TutorialChild as ChildComponent } from './TutorialChild.vue';
+
+const childMsg = ref('자식 컴포넌트로부터 아직 메시지를 받지 못했어요!');
+</script>
+
+<template>
+  <ChildComponent @response="(msg) => (childMsg = msg)" />
+  <p>{{ childMsg }}</p>
+</template>
