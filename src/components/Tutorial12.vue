bc89707cd7f14c5c1334576de31efdf4b01d6aff
Dongho Shin <kakkiid@gmail.com>
2024-09-02 16:52:34 +0900

    feat: Tutorial12, ChildComp

---
+++ src/components/Tutorial12.vue
@@ -0,0 +1,11 @@
+<!-- Props -->
+<script setup>
+import { ref } from 'vue';
+import { TutorialChild as ChildComponent } from './TutorialChild.vue';
+
+const greeting = ref('부모 컴포넌트로부터 prop을 전달받았어요!');
+</script>
+
+<template>
+  <ChildComponent :msg="greeting" />
+</template>
---
+++ src/components/TutorialChild.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  msg: String,
+});
+
+const emit = defineEmits(['response']);
+emit('response', '자식 컴포넌트로부터 받은 데이터에요!');
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
